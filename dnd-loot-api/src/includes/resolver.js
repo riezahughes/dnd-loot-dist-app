554783e126ac574ccc9ea171a8ead33f66087c54
Dan <dan@ekwholesale.co.uk>
2020-11-07 13:16:58 +0000

    Prepared statements set up

--- dnd-loot-api/src/includes/resolver.js
+++ dnd-loot-api/src/includes/resolver.js
@@ -1,27 +1,38 @@
 const resolver = {
   Query: {
     party: async (_, args, context) => {
-      const statement = `SELECT * from  party WHERE "id" = '${args.id}'`;
-      return context.pg.query(statement).then((res) => res.rows[0])
+
+      const statement = `SELECT * from  party WHERE "id" = $1`;
+      const values = [args.id];
+
+      return context.pg.query(statement, values).then((res) => res.rows[0])
         .catch((err) => console.log(err))
     },
   },
   Mutation: {
     createParty: async (_, args, context) => {
-      const statement = `INSERT INTO party("name", "createdAt") VALUES ('${args.name}', NOW()) RETURNING "name", "id", "createdAt"`;
-      return context.pg.query(statement).then((res) => res.rows[0])
+
+      const statement = `INSERT INTO party("name", "createdAt") VALUES ($1, NOW()) RETURNING "name", "id", "createdAt"`;
+      const values = [args.name]
+
+      return context.pg.query(statement, values).then((res) => res.rows[0])
         .catch((err) => console.log(err))
     },
 
     updateParty: async (_, args, context) => {
-      const statement = `UPDATE party SET "name" = '${args.name}' WHERE "id" = '${args.id}' RETURNING "name", "id", "createdAt"`;
-      return context.pg.query(statement).then((res) => res.rows[0])
+      const statement = `UPDATE party SET "name" = $1 WHERE "id" = $2 RETURNING "name", "id", "createdAt"`;
+      const values = [args.name, args.id]
+
+      return context.pg.query(statement, values).then((res) => res.rows[0])
         .catch((err) => console.log(err));
     },
 
     deleteParty: async (_, args, context) => {
-      const statement = `DELETE FROM party WHERE "id" = '${args.id}' RETURNING "name", "id", "createdAt" `;
-      return context.pg.query(statement).then((res) => res.rows[0])
+
+      const statement = `DELETE FROM party WHERE "id" = $1 RETURNING "name", "id", "createdAt" `;
+      const values = [args.id]
+
+      return context.pg.query(statement, values).then((res) => res.rows[0])
         .catch((err) => console.log(err));
     }
   }
