a9dcdd40bae26e351bb518783c17d222aa3ac7ce
Dan <dan@ekwholesale.co.uk>
2020-11-07 13:09:40 +0000

    All crud for party worked out.

--- dnd-loot-api/src/includes/resolver.js
+++ dnd-loot-api/src/includes/resolver.js
@@ -1,11 +1,29 @@
 const resolver = {
   Query: {
-    party: () => books,
+    party: async (_, args, context) => {
+      const statement = `SELECT * from  party WHERE "id" = '${args.id}'`;
+      return context.pg.query(statement).then((res) => res.rows[0])
+        .catch((err) => console.log(err))
+    },
   },
   Mutation: {
-    createParty: async (_, args, context) =>
-      context.pg.query(`INSERT INTO party("name", "createdAt") VALUES ('${args.name}', NOW()) RETURNING *`).then((res) => res.rows[0])
+    createParty: async (_, args, context) => {
+      const statement = `INSERT INTO party("name", "createdAt") VALUES ('${args.name}', NOW()) RETURNING "name", "id", "createdAt"`;
+      return context.pg.query(statement).then((res) => res.rows[0])
         .catch((err) => console.log(err))
+    },
+
+    updateParty: async (_, args, context) => {
+      const statement = `UPDATE party SET "name" = '${args.name}' WHERE "id" = '${args.id}' RETURNING "name", "id", "createdAt"`;
+      return context.pg.query(statement).then((res) => res.rows[0])
+        .catch((err) => console.log(err));
+    },
+
+    deleteParty: async (_, args, context) => {
+      const statement = `DELETE FROM party WHERE "id" = '${args.id}' RETURNING "name", "id", "createdAt" `;
+      return context.pg.query(statement).then((res) => res.rows[0])
+        .catch((err) => console.log(err));
+    }
   }
 }
 
