d1e9537811e31215d5199277d907dcf947dd14ce
Dan <dan@ekwholesale.co.uk>
2020-11-08 20:21:06 +0000

    I'm still mad at js

--- dnd-loot-api/src/includes/query/party.js
+++ dnd-loot-api/src/includes/query/party.js
@@ -33,40 +33,44 @@ const partyQuery = async (_, args, context) => {
 };
 
 const partiesQuery = async (_, args, context) => {
+
+  const returnArray = async (parties) => {
+    let partyArray = [];
+    for (let x = 0; x < parties.rows.length; x++) {
+      // console.log(res.rows);
+      const playerStatement = {
+        text: `SELECT * FROM player WHERE "party_id" = $1 ORDER BY "id" DESC LIMIT 30`,
+        values: [parties.rows[x].id]
+      };
+      // get players via party_id
+      await context.pg.query(playerStatement).then((result) => {
+        // console.log(result.rows);
+        partyArray.push({
+          id: parties.rows[x].id,
+          name: parties.rows[x].name,
+          createdAt: parties.rows[x].createdAt,
+          players: result.rows,
+        })
+      });
+
+      if (x + 1 === parties.rows.length) {
+        console.log(partyArray);
+        return partyArray;
+      }
+
+    }
+  }
+
   const statement = {
     text: 'SELECT * from party ORDER BY "id" DESC LIMIT 30',
   };
 
-
+  let finalArray = [];
   // get all rows for party.
-  return context.pg.query(statement).then((res) => {
-    let finalArray = [];
-    // console.log(res.rows);
+  return context.pg.query(statement).then(async (res) => {
     // for each party
-    // for (rowOfParty of res.rows) {
-    for (let x = 0; x < res.rows.length; x++) {
-      // console.log(res.rows);
-      const playerStatement = {
-        text: `SELECT * FROM player WHERE "party_id" = $1 ORDER BY "id" DESC LIMIT 30`,
-        values: [res.rows[x].id]
-      };
-      // get players via party_id
-      context.pg.query(playerStatement).then((result) => {
-        // console.log(result.rows);
-        return ({
-          id: res.rows[x].id,
-          name: res.rows[x].name,
-          createdAt: res.rows[x].createdAt,
-          players: result.rows,
-        })
-      }).then((res) => {
-        // console.log(res)
-        finalArray.push(res);
-        // console.log(finalArray);
-        return res;
-      });
-    }
-    return finalArray;
+    return await returnArray(res);
+
   }).then((res) => { return res }).catch((err) => console.log(err));
 };
 
