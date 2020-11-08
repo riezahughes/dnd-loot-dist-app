1774c59e4d92b5401df5e7d99e5d9af19c8bfa4c
Dan <dan@ekwholesale.co.uk>
2020-11-08 22:54:40 +0000

    It's returning at least one child. With terrible SQL

--- dnd-loot-api/src/includes/query/party.js
+++ dnd-loot-api/src/includes/query/party.js
@@ -1,33 +1,26 @@
 const partyQuery = async (_, args, context) => {
   const statement = {
-    text: 'SELECT * FROM party INNER JOIN player ON party."id" = player."party_id" WHERE party.id = $1 ORDER BY party.id DESC',
+    text: `
+    SELECT
+      party.*,
+      row_to_json(player.*) as player
+    FROM party
+    INNER JOIN player ON player."party_id" = party."id" WHERE party."id" = $1
+    ORDER BY player.id DESC`,
     values: [args.id],
-    rowMode: 'array',
   };
 
   return context.pg.query(statement).then((res) => {
-    const breakdown = res.rows[0].slice(3, res.rows[0].length);
-
-    const player = [];
-
-    for (let x = 0; x < breakdown.length; x += 5) {
-      const pushArray = {
-        id: breakdown[x],
-        name: breakdown[x + 1],
-        discord_id: breakdown[x + 2],
-        party_id: breakdown[x + 3],
-        createdAt: breakdown[x + 4],
-      };
-
-      player.push(pushArray);
-    }
-
-    return {
-      id: res.rows[0][0],
-      name: res.rows[0][1],
-      createdAt: res.rows[0][2],
-      players: player,
-    };
+    console.log(res.rows[0].player), { depth: null };
+    // console.dir(res.rows[0].player, { depth: null })
+    return ({
+      id: res.rows[0].id,
+      name: res.rows[0].name,
+      createdAt: res.rows[0].createdAt,
+      players: [
+        res.rows[0].player
+      ]
+    });
   })
     .catch((err) => console.log(err));
 };
