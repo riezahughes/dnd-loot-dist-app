f1ea7a7f87677f7043caadcd6ed1781dd758ce7f
Dan <dan@ekwholesale.co.uk>
2020-11-08 10:10:01 +0000

    Working out the right way of returning foreign data

--- dnd-loot-api/src/includes/query/party.js
+++ dnd-loot-api/src/includes/query/party.js
@@ -35,18 +35,78 @@ const partyQuery = async (_, args, context) => {
 const partiesQuery = async (_, args, context) => {
   const statement = {
     text: 'SELECT * from party INNER JOIN player ON party."id" = player."party_id" ORDER BY party."id" DESC LIMIT 30',
-    rowMode: 'array',
   };
 
   return context.pg.query(statement).then((res) => {
-    returnArray = [];
 
-    for (party in res.rows) {
-
-    }
-
+    // get all rows for party.
+    // for each party
+    // get players via party_id
+    // create array return with players and party details
+    console.log("initial rows");
     console.log(res.rows);
-    return res.rows;
+    let finalReturn = [];
+
+    res.rows.forEach((rowOfParty, index) => {
+      let playerArray = []
+
+      const playerStatement = {
+        text: `SELECT * FROM player WHERE "party_id" = $1 ORDER BY "id" DESC LIMIT 30`,
+        values: [rowOfParty.id]
+      };
+      context.pg.query(playerStatement).then((res) => {
+        console.log("Players:");
+        console.log(res.rowLength);
+        res.rows.forEach((player) => {
+          playerArray.push(player);
+        })
+        console.log(playerArray);
+        finalReturn.push({
+          id: rowOfParty.id,
+          name: rowOfParty.name,
+          createdAt: rowOfParty.createdAt,
+          players: playerArray,
+        })
+      }).then((res) => { return finalReturn });
+    });
+    // returnArray = [];
+    // console.log("Return Statement");
+    // console.log(res.rows);
+    // let currentPartyId;
+    // res.rows.forEach((rowOfParty, index) => {
+
+    //   if (index === 0) {
+    //     currentPartyId = party[0];
+    //   }
+
+    //   if (previousPartyId !== currentPartyId) {
+    //     currentPartyId = party[0];
+    //     playerArray.push(pushArray);
+    //   }
+
+    //   if (res.rows.length === )
+
+
+    //     console.log("Party Break");
+    //   console.log(party);
+
+    //   playerArray = [];
+
+    //   const breakdown = party.slice(3, party.length);
+
+    //   for (let x = 0; x < breakdown.length; x += 5) {
+    //     const pushArray = {
+    //       id: breakdown[x],
+    //       name: breakdown[x + 1],
+    //       discord_id: breakdown[x + 2],
+    //       party_id: breakdown[x + 3],
+    //       createdAt: breakdown[x + 4],
+    //     };
+    //   }
+    // });
+    // console.log("Final Returned Array");
+    // console.log(returnArray);
+    // return returnArray;
   })
     .catch((err) => console.log(err));
 };
